e890efc146b22b75e3d2400688f624c60b302842
ロイヤル <roiyaru@roiyarunoMacBook-Pro.local>
2021-03-29 20:07:21 +0900

    add_cloumn/make_seeder

---
+++ database/seeds/ContactsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('contacts')->insert([
+            'name'=>'mike',
+            'age'=>29,
+            'sex'=>'man',
+            'text'=>'質問があります',
+            'file'=>'添付ファイル',
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s')
+        ]);
+    }
+}
