9bb95e57dee2fd25e6f1325f190e6f79350a6b20
ロイヤル <roiyaru@roiyarunoMacBook-Pro.local>
2021-06-10 09:23:07 +0900

    更新

--- resources/views/contact_data/data_file.blade.php
+++ resources/views/contact_data/data_file.blade.php
@@ -11,5 +11,6 @@
 <div class="img-fluid">
     <img src="{{ $url }}" id="desert">
 </div>
+<img src="https://simpleboard1.s3.ap-northeast-1.amazonaws.com/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88+2021-05-02+18.06.54.png">
     <button class="btn btn-primary btn-lg btn-block" type="button" onclick="location.href='/data'">お問い合わせ一覧</button>
 </html>
